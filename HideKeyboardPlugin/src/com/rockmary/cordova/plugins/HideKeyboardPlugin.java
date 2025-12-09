6564d8e09805ce2d3bf75ea7a462e9801896adcc
Pablo Peña <pablo_penna7@hotmail.com>
2025-12-09 15:21:54 +0800

    feat: hide keyboard on app gaining focus

--- HideKeyboardPlugin/src/com/rockmary/cordova/plugins/HideKeyboardPlugin.java
+++ HideKeyboardPlugin/src/com/rockmary/cordova/plugins/HideKeyboardPlugin.java
@@ -1,11 +1,14 @@
 package com.rockmary.cordova.plugins;
 
+import android.app.Activity;
+import android.content.Context;
+import android.view.inputmethod.InputMethodManager;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -23,10 +26,40 @@ public class HideKeyboardPlugin extends CordovaPlugin {
     }
 
     private void coolMethod(String message, CallbackContext callbackContext) {
+        Activity activity = this.cordova.getActivity();
+        InputMethodManager imm = (InputMethodManager)activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+
         if (message != null && message.length() > 0) {
             callbackContext.success(message);
         } else {
             callbackContext.error("Expected one non-empty string argument.");
         }
     }
+
+    @Override
+    public void pluginInitialize() {
+        super.pluginInitialize();
+
+        // Hide keyboard when app starts
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+                InputMethodManager imm = (InputMethodManager) cordova.getActivity()
+                        .getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(webView.getView().getWindowToken(), 0);
+            }
+        });
+    }
+
+    @Override
+    public void onResume(boolean multitasking) {
+        // Keep keyboard hidden on resume
+        cordova.getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+                InputMethodManager imm = (InputMethodManager) cordova.getActivity()
+                        .getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(webView.getView().getWindowToken(), 0);
+            }
+        });
+    }
 }
+
